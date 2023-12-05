58e8689560214f0f97a7f2f2266471bb336bfe66
Mulan <mulanhuang75@gmail.com>
2023-12-04 22:03:26 -0800

    added more to my boostrat file

---
+++ 5-JavaScript/1-variable.js
@@ -0,0 +1,23 @@
+// sting
+let name = "John";
+console.log("The type of name:" typeof name);
+
+// number
+let age = 25;
+console.log("The type of age:", typeof age)
+
+// Boolean
+let isStudent = false;
+let isTeacher = true;
+console.log("The type of isStudent:", typeof isStudent);
+console.log("The type of isTeacher:", typeof isTeacher);
+
+// Null- Intenational absence of data
+let girlFriend = null
+console.log("The type of girlFriend:", typeof girlFriend);
+
+let city;
+console.log(city)
+console.log("The value of city:", city)
+console.log("The type of city:", typeof city)
+
